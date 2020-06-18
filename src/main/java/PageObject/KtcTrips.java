74eab0ece99d1d6e334a7169514b57c5067134f1
Bikash <bknayak9178@yahoo.com>
2020-06-18 18:34:10 +0530

    reports module update file

--- src/main/java/PageObject/KtcTrips.java
+++ src/main/java/PageObject/KtcTrips.java
@@ -1,10 +1,34 @@
 package PageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class KtcTrips {
 	public WebDriver driver;
+	Actions a;
+	
+	private By reports = By.xpath("//i[contains(text(),'assignment')]");
+	private By ktc_Trip_Reports = By.xpath("//span[contains(text(),'Ktc Trips')]");
+	private By checDriverPerformance = By.xpath("//header[contains(text(),'Ktc trips')]");
+	private By frame = By.xpath("//div[@id='modalBody_126']//iframe[@class='ifream']");
+
 	public KtcTrips(WebDriver driver) {
 		this.driver=driver;
 	}
+	public void getReports() {
+		a=new Actions(driver);
+		a.moveToElement(driver.findElement(reports)).build().perform();
+	}
+	public void getclkktc_Reports() {
+		a.moveToElement(driver.findElement(ktc_Trip_Reports)).click().build().perform();
+			
+	}
+	public String getCheck_reports() {
+		return driver.findElement(checDriverPerformance).getText();
+	}
+	public WebDriver getfream() {
+		return driver.switchTo().frame(driver.findElement(frame));
+	}
+	
 }
--- src/test/java/reports/Ktc_TripsPage.java
+++ src/test/java/reports/Ktc_TripsPage.java
@@ -2,16 +2,20 @@ package reports;
 
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.abstech.tranzack.HomePage;
 
-import PageObject.Dashboard;
+import PageObject.KtcTrips;
 import PageObject.LogInPage;
 import resource.Base;
 
 public class Ktc_TripsPage extends Base {
-	public Dashboard d;
+	public KtcTrips k;
+	SoftAssert assertion;
 	public static Logger log=org.apache.logging.log4j.LogManager.getLogger(HomePage.class.getName());
 	@BeforeTest
 	public void initialize() throws Exception {
@@ -37,6 +41,30 @@ public class Ktc_TripsPage extends Base {
 		log.info("login page login Successfully");
 		Thread.sleep(15000);
 
+	}
+	
+	@Test
+	public void clk_Ktc_Reports() throws InterruptedException {
+		k=new KtcTrips(driver);
+		k.getReports();
+		log.info("Successfully enter reports module");
+		k.getclkktc_Reports();
+		String printktcreport=k.getCheck_reports();
+		System.out.println(printktcreport);
+		log.info("Successfully enter ktc report module");
+		assertion=new SoftAssert();
+		assertion.assertEquals(printktcreport, "Ktc trips");
+		System.out.println("checking ktc trip reports");
+		Thread.sleep(5000);
+		k.getfream();
+		log.info("Successfully enter frame");
+		Thread.sleep(5000);
+		
+	}
+	@AfterTest
+	public void teardown() {
+		driver.quit();
+		driver = null;
 	}
 
 }
